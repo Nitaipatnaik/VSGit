f5cd5268fa24b1e66eb5cb0d84f1bc297e3104a5
Nitai Patnaik <nitai.patnaik@siemens-healthineers.com>
2020-02-24 16:04:14 +0530

    Update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
             //Update from VS
             //Update from Feature::VS
             //Update from Feature::GitHub
+            //Update from Feature::VS2
         }
     }
 }
